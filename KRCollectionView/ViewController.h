7c16ad348c46d5afdc32e874930d614927cc0148
gurongkang <1530669303@qq.com>
2018-07-03 16:24:44 +0800

    初始化项目

---
+++ KRCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KRCollectionView
+//
+//  Created by RK on 2018/7/3.
+//  Copyright © 2018年 RK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KRCollectionViewTests/KRCollectionViewTests.m
@@ -0,0 +1,39 @@
+//
+//  KRCollectionViewTests.m
+//  KRCollectionViewTests
+//
+//  Created by RK on 2018/7/3.
+//  Copyright © 2018年 RK. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KRCollectionViewTests : XCTestCase
+
+@end
+
+@implementation KRCollectionViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
